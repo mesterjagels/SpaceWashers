c564354fb7a587c0b1be2af2a30d9a4af805c52c
Pearlcorder <AndreasLagerstedt@Andreass-MacBook-Pro-2.local>
2016-03-15 12:17:17 +0100

    no message

--- Assets/Scripts/RagdollTest.cs
+++ Assets/Scripts/RagdollTest.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class RagdollTest : MonoBehaviour {
 
+	public float force = 10;
 	private Rigidbody2D rb;
 
 	void Awake () {
@@ -18,16 +19,20 @@ public class RagdollTest : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	
-		if (Input.GetKeyDown ("w")) {
-			rb.velocity = new Vector2(rb.velocity.x, 20);
+		if (Input.GetKey ("w")) {
+			rb.velocity = new Vector2(rb.velocity.x, force);
 		}
 
-		if (Input.GetKeyDown ("a")) {
-			rb.velocity = new Vector2(-20, rb.velocity.y);
+		if (Input.GetKey ("a")) {
+			rb.velocity = new Vector2(-force, rb.velocity.y);
 		}
 
-		if (Input.GetKeyDown ("d")) {
-			rb.velocity = new Vector2(20, rb.velocity.y);
+		if (Input.GetKey ("d")) {
+			rb.velocity = new Vector2(force, rb.velocity.y);
+		}
+
+		if (Input.GetKey ("s")) {
+			rb.velocity = new Vector2(rb.velocity.x, -force);
 		}
 	}
 }
