d1aa85fa0aa7c33c84d175890db7945b7c4b0864
Jasper van't Veen <jvanveen@gmail.com>
2016-03-17 11:27:16 +0100

    Huge commit. Overwritten everything

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,18 +4,20 @@ using System.Collections;
 public class Movement : MonoBehaviour {
 
 	public KeyCode left, right, up, down, magnetBoots, detach;
-	public float moveSpeed, nonRbSpeed, bracingSlow;
+	public float moveSpeed, moveSpeedWBoots, bracingSlow;
 	Transform tf;
 	Vector3 movePos;
 	bool magnet;
 	float slow;
-	Rigidbody2D rb;
+	Rigidbody2D rb, spaceshipRb;
 	public Vector3 curVelocity;
 	Vector2 velX, velZ;
 	public Transform cord;
 	LineRenderer line;
 	public float distToCord;
-
+	public bool washing;
+	bool moving;
+//	GameObject spaceship;
 	// Use this for initialization
 	void Start () {
 		tf = gameObject.transform;
@@ -23,25 +25,31 @@ public class Movement : MonoBehaviour {
 		rb = gameObject.GetComponent<Rigidbody2D> ();
 		slow = 1;
 		magnet = false;
+//		spaceship = GameObject.FindGameObjectWithTag("Spaceship");
+		spaceshipRb = GameObject.FindGameObjectWithTag("Spaceship").GetComponent<Rigidbody2D>();
 //		line = gameObject.GetComponent<LineRenderer>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKey (left)) {
+		if (Input.GetKey (left) || Input.GetAxis("Horizontal") < 0) {
 			if (!magnet) {
 //				rb.AddForce (Vector2.left * moveSpeed * slow * Time.deltaTime);
 				velX += (Vector2.left * moveSpeed * slow * Time.deltaTime * 0.2f);
 				MoveRB ();
 			} else {
-				movePos.x -= (nonRbSpeed * slow * Time.deltaTime);
-				Move ();
+//				movePos.x -= (moveSpeedWBoots * slow * Time.deltaTime);
+//				Move ();
+				velX += (Vector2.left * moveSpeedWBoots * slow * Time.deltaTime * 0.2f);
+				MoveRB ();
 			}
-		} else if (Input.GetKeyUp (left)) {
+		} else if (Input.GetKeyUp (left) || Input.GetAxis("Horizontal") == 0) {
 			velX = Vector2.zero;
+			if (moving)
+				moving = false;
 		}
 
-		if (Input.GetKey (right)) 
+		if (Input.GetKey (right) || Input.GetAxis("Horizontal") > 0) 
 		{
 			if (!magnet) 
 			{
@@ -50,45 +58,57 @@ public class Movement : MonoBehaviour {
 				MoveRB ();
 			} else 
 			{
-				movePos.x += (nonRbSpeed * slow * Time.deltaTime);
-				Move ();
+//				movePos.x += (moveSpeedWBoots * slow * Time.deltaTime);
+//				Move ();
+				velX += (Vector2.right * moveSpeedWBoots * slow * Time.deltaTime*0.2f);
+				MoveRB ();
 			}
 
 
-		}else if (Input.GetKeyUp (right)) {
+		}else if (Input.GetKeyUp (right) || Input.GetAxis("Horizontal") == 0) {
 			velX = Vector2.zero;
+			if (moving)
+				moving = false;
 		}
 
-		if (Input.GetKey (up)) 
+		if (Input.GetKey (up) || Input.GetAxis("Vertical") > 0) 
 		{
 			if (!magnet) 
 			{
 //				rb.AddForce (Vector3.forward * moveSpeed * slow * Time.deltaTime);
-				velZ += Vector2.up * moveSpeed * slow * Time.deltaTime*0.2f;
+				velZ += (Vector2.up * moveSpeed * slow * Time.deltaTime*0.2f);
 				MoveRB ();
 			} else 
 			{
-				movePos.z += (nonRbSpeed * slow * Time.deltaTime);
-				Move ();
+				velZ += (Vector2.up * moveSpeedWBoots * slow * Time.deltaTime*0.2f);
+				MoveRB ();
+//				movePos.z += (moveSpeedWBoots * slow * Time.deltaTime);
+//				Move ();
 			}
-		}else if (Input.GetKeyUp (up)) {
+		}else if (Input.GetKeyUp (up) || Input.GetAxis("Vertical") == 0) {
 			velZ = Vector2.zero;
+			if (moving)
+				moving = false;
 		}
 
-		if (Input.GetKey (down)) 
+		if (Input.GetKey (down) || Input.GetAxis("Vertical") < 0) 
 		{
 			if (!magnet) 
 			{
 //				rb.AddForce (Vector3.back * moveSpeed * slow * Time.deltaTime);
-				velZ += Vector2.down * moveSpeed * slow * Time.deltaTime*0.2f;
+				velZ += (Vector2.down * moveSpeed * slow * Time.deltaTime*0.2f);
 				MoveRB ();
 			} else 
 			{
-				movePos.z -= (nonRbSpeed * slow * Time.deltaTime);
-				Move ();
+				velZ += (Vector2.down * moveSpeedWBoots * slow * Time.deltaTime*0.2f);
+				MoveRB ();
+//				movePos.z -= (moveSpeedWBoots * slow * Time.deltaTime);
+//				Move ();
 			}
-		}else if (Input.GetKeyUp (down)) {
+		}else if (Input.GetKeyUp (down) || Input.GetAxis("Vertical") == 0) {
 			velZ = Vector2.zero;
+			if (moving)
+				moving = false;
 		}
 
 		if (magnet && rb.velocity != Vector2.zero) {
@@ -98,16 +118,24 @@ public class Movement : MonoBehaviour {
 		if (Input.GetKeyDown (magnetBoots) )
 		{
 			Invoke ("Boots", 1.5f);
-
 		}
 
-		if (Input.GetKeyDown (detach)) {
+		if (Input.GetAxis ("HorizontalR") != 0 && Input.GetAxis("VerticalR") != 0) {
+			washing = true;
+		}else{
+			washing = false;
+		}
+
+		if (Input.GetKey (detach)) {
 			cord.gameObject.GetComponent<RopeStart> ().Detach (tf.position);
 		}
 //		line.SetPosition (0, tf.position);
 //		line.SetPosition (1, cord.position);
 		distToCord = Vector3.Distance (tf.position, cord.position);
 //		curVelocity = rb.velocity;
+		if (!moving){
+			rb.velocity = spaceshipRb.velocity;
+		}
 	}
 
 	void Move () {
@@ -115,15 +143,16 @@ public class Movement : MonoBehaviour {
 	}
 
 	void MoveRB () {
-		rb.velocity = new Vector2 (velX.x, velZ.y);
+		rb.velocity = spaceshipRb.velocity + new Vector2 (velX.x, velZ.y);
+		moving = true;
 	}
 
 	void Boots () {
 		if (magnet) 
 		{
 			slow = 1;
-			rb.velocity = Vector2.zero;
-			rb.angularVelocity = 0;
+			rb.velocity = Vector2.zero+spaceshipRb.velocity;
+			rb.angularVelocity = 0+spaceshipRb.angularVelocity;
 			magnet = false;
 		} else 
 		{
--- Assets/Scripts/SpaceshipController.cs
+++ Assets/Scripts/SpaceshipController.cs
@@ -1,7 +1,20 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
+using Uniduino;
 
 public class SpaceshipController : MonoBehaviour {
+
+	public Arduino arduino;
+	public int pinUp = 2;
+	public int pinDown = 3;
+	public int pinLeft = 4;
+	public int pinRight = 5;
+	public int pinBtn1 = 6;
+	public int pinBtn2 = 7;
+
+	public int pinBtn1Light = 8;
+	public int pinBtn2Light = 9;
 
 	public Vector2 startPos, lPos, rPos;
 	public Vector3 targetPos;
@@ -11,13 +24,21 @@ public class SpaceshipController : MonoBehaviour {
 	float moveTime, rotTime;
 	public AnimationClip left, right;
 	Animation anim;
-	public float moveSpeed, laneSwitchSpeed;
+	public float minSpeed, maxSpeed;
+	public float moveSpeed, laneSwitchSpeed, sideThrustSpeed, sideDecelerationSpeed, sideThrustAccelerationSpeed;
 	float moveTo;
+	public Vector2 sideVel;
 	public Vector2 velocity;
 	Rigidbody2D rb;
+	public bool gridMovement;
+	public bool decel;
+	float sideAccTimer;
+	public bool sideMove;
+	public Text distTravelled, timeElapsed;
 //	public float vibSpeed;
 //	public float timeToMove;
 	// Use this for initialization
+
 	void Start () {
 		tf = gameObject.transform;
 		startPos = tf.position;
@@ -26,18 +47,40 @@ public class SpaceshipController : MonoBehaviour {
 		rPos = new Vector3 (startPos.x+10, startPos.y);
 		anim = GetComponent<Animation>();
 		rb = GetComponent<Rigidbody2D> ();
+		sideAccTimer = 0;
+
+		arduino = Arduino.global;
+		arduino.Setup(ConfigurePins);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetMouseButtonDown (0)) {
+		if (Input.GetMouseButton (0) | arduino.digitalRead (pinLeft) == 1) {
 			GoLeft();
+			sideMove = true;
 		}
-		if (Input.GetMouseButtonDown (1)) {
+		if (Input.GetMouseButtonUp(0) && !gridMovement && rb.velocity.x > 0){
+			decel = true;
+			sideMove = false;
+		}
+		if (Input.GetMouseButton (1) | arduino.digitalRead (pinRight) == 1) {
 			GoRight ();
+			sideMove = true;
+		}
+		if (Input.GetMouseButtonUp(1) && !gridMovement && rb.velocity.x < 0){
+			decel = true;
+			Debug.Log ("decelerate");
+			sideMove = false;
 		}
 
-		if (move) {
+		if (Input.GetAxis("Mouse ScrollWheel") > 0 && moveSpeed < maxSpeed){
+			moveSpeed ++;
+		}else if (Input.GetAxis("Mouse ScrollWheel") < 0 && moveSpeed > minSpeed){
+			moveSpeed --;
+		}
+
+
+		if (move && gridMovement) {
 			moveTime += Time.deltaTime*0.25f;
 			rotTime = moveTime*2;
 			if (tf.position.x != targetPos.x) {
@@ -56,14 +99,53 @@ public class SpaceshipController : MonoBehaviour {
 			}
 		}
 
+		if (decel) {
+			Decelerate();
+		}
+		if (sideMove){
+			sideAccTimer += Time.deltaTime*sideThrustAccelerationSpeed;
+		}else {
+			sideAccTimer = 0;
+		}
+
 		moveTo = (Time.deltaTime * moveSpeed);
 //		tf.position += Vector3.up * moveTo;
 //		moveSpeed += Time.deltaTime*0.1f;
-		rb.velocity = new Vector2 (0,moveSpeed);
+		rb.velocity = new Vector2 (rb.velocity.x,moveSpeed);
 		velocity = GetComponent<Rigidbody2D> ().velocity;
 //		Quaternion vibRot = new Quaternion (tf.rotation.x, 0.5f+(Mathf.PingPong (Time.time * vibSpeed,1)), tf.rotation.z, tf.rotation.w);
 //		tf.rotation = vibRot;
 //		transform.rotation.x = Mathf.Sin (Time.time * vibSpeed);
+		distTravelled.text = (tf.position.y-startPos.y).ToString();
+		timeElapsed.text += Time.unscaledDeltaTime.ToString();
+
+	}
+
+	void Decelerate () {
+		if (rb.velocity.x < 0){
+			rb.velocity = new Vector2 (rb.velocity.x+(sideDecelerationSpeed*Time.deltaTime), rb.velocity.y);
+		}else {
+			rb.velocity = new Vector2 (rb.velocity.x-(sideDecelerationSpeed*Time.deltaTime), rb.velocity.y);
+		}
+		if (rb.velocity.x == 0) {
+			decel = false;
+		}
+	}
+
+	//Method for setting up the different pins
+	void ConfigurePins()
+	{
+		arduino.pinMode(pinUp, PinMode.INPUT);
+		arduino.pinMode(pinDown, PinMode.INPUT);
+		arduino.pinMode(pinLeft, PinMode.INPUT);
+		arduino.pinMode(pinRight, PinMode.INPUT);
+		arduino.pinMode(pinBtn1, PinMode.INPUT);
+
+		arduino.pinMode(pinBtn1Light, PinMode.OUTPUT);
+
+		//Only need to activate once for one pin, becuase all pins are on the same Port
+		arduino.reportDigital((byte)(pinUp / 8), 1);
+
 	}
 
 	void GoLeft () {
@@ -76,10 +158,19 @@ public class SpaceshipController : MonoBehaviour {
 				targetPos = new Vector3 (startPos.x, tf.position.y, tf.position.z);
 			}
 
-			anim.clip = left;
-			anim.Play ();
+//			anim.clip = left;
+//			anim.Play ();
 			move = true;
 
+		}
+		if (!gridMovement) {
+			Debug.Log ("left");
+			if (sideVel.x > (-sideThrustSpeed)){
+				sideVel = Vector2.left*sideThrustSpeed*sideAccTimer;
+			}else {
+				sideVel = Vector2.left*sideThrustSpeed;
+			}
+			rb.velocity = new Vector2 (sideVel.x, rb.velocity.y);
 		}
 	}
 
@@ -92,9 +183,20 @@ public class SpaceshipController : MonoBehaviour {
 //				startPos = new Vector3 (startPos.x, startPos.y, tf.position.z);
 				targetPos = new Vector3 (startPos.x, tf.position.y, tf.position.z);
 			}
-			anim.clip = right;
-			anim.Play ();
+//			anim.clip = right;
+//			anim.Play ();
 			move = true;
 		}
+		if (!gridMovement) {
+			sideVel = Vector2.right*sideThrustSpeed;
+			if (sideVel.x < (sideThrustSpeed)){
+				sideVel = Vector2.right*sideThrustSpeed*sideAccTimer;
+			}else {
+				sideVel = Vector2.right*sideThrustSpeed;
+			}
+			rb.velocity = new Vector2 (sideVel.x, rb.velocity.y);
+		}
 	}
+
+
 }
