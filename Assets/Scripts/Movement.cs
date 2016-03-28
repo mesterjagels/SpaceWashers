a47dee76face8c802fcdeada9cf9314dafd9adfd
Simen Rytter Johansen <sijo@itu.dk>
2016-03-28 18:51:19 +0200

    Added controller input support for multiple washers.

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -18,6 +18,8 @@ public class Movement : MonoBehaviour {
 	public bool washing;
 	public bool moving;
 	public float normalDrag, magnetDrag;
+	int playerNr;
+	string lHorAxis, rHorAxis, lVerAxis, rVerAxis;
 //	GameObject spaceship;
 	// Use this for initialization
 	void Start () {
@@ -29,13 +31,37 @@ public class Movement : MonoBehaviour {
 //		spaceship = GameObject.FindGameObjectWithTag("Spaceship");
 		spaceshipRb = GameObject.FindGameObjectWithTag("Spaceship").GetComponent<Rigidbody2D>();
 		rb.drag = normalDrag;
+		if (tf.parent.parent.name == "Player1") {
+			playerNr = 1;
+		}else if (tf.parent.parent.name == "Player2") {
+			playerNr = 2;
+		}else if(tf.parent.parent.name == "Player3") {
+			playerNr = 3;
+		}
+
+		if (playerNr == 1) {
+			lHorAxis = "Horizontal";
+			lVerAxis = "Vertical";
+			rHorAxis = "HorizontalR1";
+			rVerAxis = "VerticalR1";
+		} else if (playerNr == 2) {
+			lHorAxis = "HorizontalL1";
+			lVerAxis = "VerticalL1";
+			rHorAxis = "HorizontalR2";
+			rVerAxis = "VerticalR2";
+		} else if (playerNr == 3) {
+			lHorAxis = "HorizontalL2";
+			lVerAxis = "VerticalL2";
+			rHorAxis = "HorizontalR3";
+			rVerAxis = "VerticalR3";
+		}
 //		line = gameObject.GetComponent<LineRenderer>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKey (left)
-//		    || Input.GetAxis("Horizontal") < 0
+//		    || Input.GetAxis(lHorAxis) < 0
 		    ) {
 			if (!magnet) {
 //				rb.AddForce (Vector2.left * moveSpeed * slow * Time.deltaTime);
@@ -48,7 +74,7 @@ public class Movement : MonoBehaviour {
 				MoveRB ();
 			}
 		} else if (Input.GetKeyUp (left)
-//		           || Input.GetAxis("Horizontal") == 0
+//		           || Input.GetAxis(lHorAxis) == 0
 		           ) {
 			velX = Vector2.zero;
 			if (moving)
@@ -56,7 +82,7 @@ public class Movement : MonoBehaviour {
 		}
 
 		if (Input.GetKey (right)
-//		    || Input.GetAxis("Horizontal") > 0
+//		    || Input.GetAxis(lHorAxis) > 0
 		    ) {
 			if (!magnet) 
 			{
@@ -73,7 +99,7 @@ public class Movement : MonoBehaviour {
 
 
 		}else if (Input.GetKeyUp (right)
-//		          || Input.GetAxis("Horizontal") == 0
+//		          || Input.GetAxis(lHorAxis) == 0
 		          ) {
 			velX = Vector2.zero;
 			if (moving)
@@ -81,7 +107,7 @@ public class Movement : MonoBehaviour {
 		}
 
 		if (Input.GetKey (up)
-//		    || Input.GetAxis("Vertical") > 0
+//		    || Input.GetAxis(lVerAxis) > 0
 		    ) {
 			if (!magnet) 
 			{
@@ -96,7 +122,7 @@ public class Movement : MonoBehaviour {
 //				Move ();
 			}
 		}else if (Input.GetKeyUp (up)
-//		          || Input.GetAxis("Vertical") == 0
+//		          || Input.GetAxis(lVerAxis) == 0
 		          ) {
 			velZ = Vector2.zero;
 			if (moving)
@@ -104,7 +130,7 @@ public class Movement : MonoBehaviour {
 		}
 
 		if (Input.GetKey (down)
-//		    || Input.GetAxis("Vertical") < 0
+//		    || Input.GetAxis(lVerAxis) < 0
 		    ) {
 
 			if (!magnet) 
@@ -120,7 +146,7 @@ public class Movement : MonoBehaviour {
 //				Move ();
 			}
 		}else if (Input.GetKeyUp (down)
-//		          || Input.GetAxis("Vertical") == 0
+//		          || Input.GetAxis(lVerAxis) == 0
 		          ) {
 			velZ = Vector2.zero;
 			if (moving)
@@ -136,7 +162,7 @@ public class Movement : MonoBehaviour {
 			Invoke ("Boots", 1.5f);
 		}
 
-		if (Input.GetAxis ("HorizontalR") != 0 || Input.GetAxis("VerticalR") != 0) {
+		if (Input.GetAxis (rHorAxis) != 0 || Input.GetAxis(rVerAxis) != 0) {
 			washing = true;
 		}else{
 			washing = false;
