11e3eeec217042fb3821175905d6049840f4a4b3
Jasper van't Veen <jvanveen@gmail.com>
2016-04-07 14:42:56 +0200

    UI stuff

---
+++ Assets/LockUIItem.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LockUIItem : MonoBehaviour
+{
+    private Vector3 position;
+    private Vector3 scale;
+    // Use this for initialization
+    void Awake()
+    {
+        position = this.gameObject.GetComponent<RectTransform>().position;
+        scale = this.gameObject.GetComponent<RectTransform>().localScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (position != this.gameObject.GetComponent<RectTransform>().position)
+        {
+            this.gameObject.GetComponent<RectTransform>().position = position;
+            this.gameObject.GetComponent<RectTransform>().localScale = scale;
+        }
+    }
+}
